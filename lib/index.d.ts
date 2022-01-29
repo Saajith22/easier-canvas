1f2d53580ea6e23c8f191079b159a650d68bfb00
Saajith22 <mohammedsaajith93@gmail.com>
2022-01-29 14:56:14 +0400

    type declaration fixes

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,1 +1,3 @@
-export declare const Greeter: (name: string) => string;
+/// <reference types="node" />
+import { CanvasRenderingContext2D } from 'canvas';
+export declare const bg: (img: Buffer, ctx: CanvasRenderingContext2D, x: number, y: number) => Promise<void>;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { Image, CanvasRenderingContext2D, loadImage} from 'canvas';
+import { CanvasRenderingContext2D, loadImage } from 'canvas';
 
-export const bg = async (img: Image, ctx: CanvasRenderingContext2D, x: number, y: number) => {
+export const bg = async (img: Buffer, ctx: CanvasRenderingContext2D, x: number, y: number) => {
   if (!img) throw new Error('Image has not been provided!!');
   if (!ctx) throw new Error('Ctx has not been provided!!');
 
